c5b72c7e1a0b84da2cbf26ff3e9d11cae681ad2d
Sathish Kumar <neosathishgct@gmail.com>
2018-08-26 18:36:50 +0530

    updated to lastest bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
