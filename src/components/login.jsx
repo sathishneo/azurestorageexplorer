1966743bc189810ad0facb4b87af57ec217ca272
Sathishkumar Subramanian <neosathishgct@gmail.com>
2018-09-06 07:15:53 +0530

    dropdown to choose the storage service

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,7 +5,32 @@ class Login extends Component {
   render() {
     return (
       <div id="loginComponent">
-        <textarea id="username" />
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-danger dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Action
+          </button>
+          <div className="dropdown-menu">
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+            <a className="dropdown-item" href="#">
+              Something else here
+            </a>
+            <div className="dropdown-divider" />
+            <a className="dropdown-item" href="#">
+              Separated link
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
