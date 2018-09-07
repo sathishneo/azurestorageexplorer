55df978352e3c814a21ebe95e7d408a8acef1e43
Sathishkumar Subramanian <neosathishgct@gmail.com>
2018-09-07 16:07:11 +0530

    logincomponent div

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,32 +4,30 @@ class Login extends Component {
   state = {};
   render() {
     return (
-      <div id="loginComponent">
-        <div className="btn-group">
-          <button
-            type="button"
-            className="btn btn-danger dropdown-toggle"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
+      <div id="loginComponent" className="btn-group">
+        <button
+          type="button"
+          className="btn btn-danger dropdown-toggle"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Action
+        </button>
+        <div className="dropdown-menu">
+          <a className="dropdown-item" href="#">
             Action
-          </button>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="#">
-              Action
-            </a>
-            <a className="dropdown-item" href="#">
-              Another action
-            </a>
-            <a className="dropdown-item" href="#">
-              Something else here
-            </a>
-            <div className="dropdown-divider" />
-            <a className="dropdown-item" href="#">
-              Separated link
-            </a>
-          </div>
+          </a>
+          <a className="dropdown-item" href="#">
+            Another action
+          </a>
+          <a className="dropdown-item" href="#">
+            Something else here
+          </a>
+          <div className="dropdown-divider" />
+          <a className="dropdown-item" href="#">
+            Separated link
+          </a>
         </div>
       </div>
     );
